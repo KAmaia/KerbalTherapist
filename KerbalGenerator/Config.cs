4c688b14a9754ae9199ae80c8a12b99c820ba850
Kamaia <krystalamaia@gmail.com>
2016-03-24 21:06:37 -0500

    trying that again

---
+++ KerbalGenerator/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace KerbalGenerator {
+	public class Config {
+		public Dictionary<string, string> SavePaths { get; set; }
+		public string KSPPath { get; set; }
+		public string Name { get; set; }
+	}
+}
