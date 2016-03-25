2e7c6ea0fe94219874187944816ae7fe4348b64d
Kamaia <krystalamaia@gmail.com>
2016-03-24 21:59:25 -0500

    fuck you git.  Fuck you microsoft

---
+++ KerbalGenerator/PersistentNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KerbalGenerator {
+	class PersistentNode {
+		List<PersistentNode> nodes;
+		string name;
+		private string v;
+
+		public string Name { get; internal set; }
+
+		public PersistentNode() {
+			this.name = name;
+
+		}
+
+		public PersistentNode( string v ) {
+			this.v = v;
+		}
+
+		internal void AddValue( string pairKey, string pairValue ) {
+			throw new NotImplementedException( );
+		}
+	}
+}
